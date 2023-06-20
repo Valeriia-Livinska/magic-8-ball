e6df2555d9bee7593804bd73e973d0a44b06f2e7
Valeriia-Livinska <valerylivi@gmail.com>
2023-06-20 16:19:27 +0200

    set initial settings

---
+++ src/theme.js
@@ -0,0 +1,44 @@
+export const theme = {
+  colors: {
+    black: "#000",
+    white: "#fff",
+    darkBlue: "#0A1828",
+    turquoise: "#178582",
+    gold: "#BFA181",
+    text: "#fff",
+    textColorized: "#E0144C",
+    // border: "darkgrey",
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: "system-ui, sans-serif",
+    heading: "system-ui, sans-serif",
+    monospace: "Menlo, monospace",
+  },
+  fontSizes: {
+    xs: "12px",
+    s: "14px",
+    m: "16px",
+    l: "32px",
+    xl: "64px",
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: "none",
+    normal: "1px solid",
+    medium: "2px solid",
+    large: "3px solid",
+  },
+  radii: {
+    none: "0",
+    normal: "4px",
+    round: "50%",
+  },
+};
