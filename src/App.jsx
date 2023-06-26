269358073b5a0872c030c05b44a8552c6f0c968b
Valeriia-Livinska <valerylivi@gmail.com>
2023-06-26 16:20:22 +0200

    old version of browser router

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+
+const HomePage = lazy(() => import("./pages/Home/Home.jsx"));
+const LoginPage = lazy(() => import("./pages/Login/Login.jsx"));
+const QnAListPage = lazy(() => import("./pages/Q&A List/QnAList.jsx"));
+const Register = lazy(() => import("./pages/Register/Register.jsx"));
+const NotFound = lazy(() => import("./pages/Not found/NotFound.jsx"));
+
+// try to use replace property while using a login page
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/qnalist" element={<QnAListPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,18 +4,17 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+
+// Configure application to run underneath a specific basename in the URL by using basename="/magic-8-ball" in BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
