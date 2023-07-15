7bb90b74610c5ae3fb1bceab914148f499b048e3
Valeriia-Livinska <valerylivi@gmail.com>
2023-07-15 21:21:22 +0200

    fix env variables

--- src/components/Helpers/api.js
+++ src/components/Helpers/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.RECT_APP_BASE_URL;
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 export const fetchAnswers = async () => {
   const res = await axios.get(`/fortune-telling`);
